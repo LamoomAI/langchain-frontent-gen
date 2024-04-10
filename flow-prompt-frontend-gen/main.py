a175acce47aab447bdcba5c915351ac049a7e4f1
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2024-04-10 23:31:54 +0300

    feat: cleaned up redundancies

--- flow-prompt-frontend-gen/main.py
+++ flow-prompt-frontend-gen/main.py
@@ -1,44 +1,96 @@
 from flow_prompt import FlowPrompt, PipePrompt
-from flow_prompt import AIModelsBehaviour, AzureAIModel, AttemptToCall
-import os
+from flow_prompt import AIModelsBehaviour, AzureAIModel, OpenAIModel, AttemptToCall
+from flow_prompt import OPENAI_GPT3_5_TURBO_0125_BEHAVIOUR
+from flow_prompt import C_128K 
+import os, sys
 import dotenv
+import json
+
+from tree import TreeNode, build_tree_from_indented_text
+import file_manager
+
 
 class FrontendAgent:
 	def __init__(self) -> None:
-		fp_api_token = os.environ['FLOW_PROMPT_API_KEY']
-		# openai_key = os.environ['']
-		self.__llm_instance = FlowPrompt(api_token=fp_api_token, openai_key=)
+		azure_keys = json.loads(os.getenv("AZURE_KEYS", "{}"))
+		self.__llm_instance = FlowPrompt(azure_keys=azure_keys)
 		self.__project_prompt = PipePrompt('generate_react_project_structure')
 		self.__project_prompt.add(content='Create the project folder structure for a {app_name} web application \
-												in React. Use double spaces for indentation', role='system')
+											in React. Use double spaces for indentation and no other symbols.\
+											Make sure there`s only one root directory src which includes components and pages directories.')
 		self.__component_prompt = PipePrompt('generate_react_component')
-		self.behavior = AIModelsBehaviour(
-			attempts=[
-				AttemptToCall(
-					ai_model=AzureAIModel(
-						realm="westus",
-						deployment_id="gpt-4-turbo",
-						max_tokens=40_000,
-						support_functions=True,
-						should_verify_client_has_creds=False,
-					),
-					weight=100,
-				),
-			]
-		)
-		
+		self.__component_prompt.add(content='write a react component for a {component_name}. You may use tailwind and lucide icons for styling')
+		self.__page_prompt = PipePrompt('generate_react_page_aggregate_component')
+		self.__page_prompt.add('write a react component for a {page_name} page. You may use {component_list} components \
+								from {comp_dir_path} if any of these belong on the page')
+		self.behavior = OPENAI_GPT3_5_TURBO_0125_BEHAVIOUR
+
 
 	def generate_project_structure(self, proj_name: str) -> str:
 		proj_context = {'app_name': proj_name}
-		llm_response = self.__llm_instance.call(self.__project_prompt.id, proj_context, self.behavior)
+		structure_response = self.__llm_instance.call(self.__project_prompt.id, proj_context, self.behavior)
+		return structure_response.content
 		
 
 	def generate_component(self, comp_name: str) -> str:
-		comp_prompt_text = 'write a react component for a {component_name}'.format(component_name=comp_name)
-		self.__component_prompt.add()
-		return
+		comp_context = {'component_name': comp_name}
+		comp_response = self.__llm_instance.call(self.__component_prompt.id, comp_context, self.behavior)
+		return comp_response.content
+
+	def generate_page(self, page_name: str, exsisting_components: list[str], path_to_components: str) -> str:
+		ctx = {'page_name': page_name, 'component_list': exsisting_components, 'comp_dir_path': path_to_components}
+		page_response = self.__llm_instance.call(self.__page_prompt.id, ctx, self.behavior)
+		return page_response.content
+
+	def generate_component_batch(self, dir_path: str, component_names: list[str]):
+		for component_name in component_names:
+			code = self.generate_component(component_name)
+			file_manager.create_file(dir_path, f'{component_name}.js', code)
+
+	def generate_page_batch(self, dir_path: str, page_names: list[str], children_components: list[str], path_to_components: str):
+		for page_name in page_names:
+			code = self.generate_page(page_name, children_components, path_to_components)
+			file_manager.create_file(dir_path, f'{page_name}.js', code)
+
+
+def extract_file_name_no_ext(full_name: str, extension: str) -> str:
+	return full_name.split(extension)[0]
+
+def extract_component_names(agent: FrontendAgent, proj_tree: TreeNode) -> list[str]:
+	component_names = []
+	for component_file in proj_tree.children:
+		# Check if it's a leaf
+		if len(component_file.children) == 0:
+			if component_file.name.endswith('js') or component_file.name.endswith('jsx'):
+				component_name = extract_file_name_no_ext(component_file.name, '.js')
+				component_names.append(component_name)
+		else:
+			component_names.extend(extract_component_names(agent=agent, proj_tree=component_file))
+	return component_names
+
 
 if __name__ == '__main__':
 	dotenv.load_dotenv(dotenv.find_dotenv())
 	agent = FrontendAgent()
-	print(agent.generate_project_structure(proj_name='chat app'))
+	proj_structure = agent.generate_project_structure(proj_name='chat app')
+	
+	proj_tree = build_tree_from_indented_text(proj_structure)
+	proj_tree.pre_order_traverse()
+ 
+	components_dir_subtree = proj_tree.find_node_by_name('components')
+	pages_dir_subtree = proj_tree.find_node_by_name('pages')
+	if components_dir_subtree is None:
+		raise Exception('components dir is missing')
+	
+	
+	component_names = extract_component_names(agent=agent, proj_tree=components_dir_subtree)
+	pages_names = extract_component_names(agent=agent, proj_tree=pages_dir_subtree)
+	print(component_names)
+	print(pages_names)
+	
+	components_dir_path = file_manager.create_overwrite_directory('./temp', 'components')
+	pages_dir_path = file_manager.create_overwrite_directory('./temp', 'pages')
+	agent.generate_component_batch(components_dir_path, component_names)
+	agent.generate_page_batch(pages_dir_path, pages_names, component_names, '../components')
+
+ 
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import asyncio
-import json
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-import getpass
-import os
 import dotenv
 
 from tree import TreeNode, build_tree_from_indented_text
@@ -12,21 +9,14 @@ import file_manager
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-"""
-Steps to successful frontend gen
-  1. decompose into components (depth:null), form VDOM
-  2. generate code for components from leaves up the tree
-  3. structurize & generate package file
-  4. build
-"""
 
 class FrontendAgent:
 	def __init__(self):
 		self.__llm_instance = OpenAI(max_tokens=3900)
 		self.__component_prompt_template = PromptTemplate(input_variables=['component'], template='Write a React functional component for {component}. Please use tailwind and Lucide icons. Do not use descriptions')
 		self.__project_prompt_template = PromptTemplate(input_variables=['project_name'], template=prompt_templates.project_structure_template)
-		self.__optimize_prompt = 'Can you rewrite the code so that there is less code and results are untouched'
-		self.pages_prompt = PromptTemplate(input_variables=['page_name', 'project_name', 'components'], input_types={'page_name': str, 'project_name': str, 'components': list[str]}, template='Create a {page_name} page for {project_name}. You may use {components} if any of these belong on the page. Don`t explain anything, just write code')
+		self.pages_prompt = PromptTemplate(input_variables=['page_name', 'project_name', 'components'], input_types={'page_name': str, 'project_name': str, 'components': list[str]}, template='Create a {page_name} \
+																		 react page component for {project_name}. You may use {components} if any of these belong on the page. Don`t explain anything, just write code. All components are located in ../components directory')
 		self.pages_chain = self.pages_prompt | self.__llm_instance
 
 	def generate_project_structure(self, project_name: str):
@@ -43,10 +33,6 @@ class FrontendAgent:
 		component_prompts = [self.__component_prompt_template.format(component=prompt) for prompt in prompts]
 		generated_code = self.__llm_instance.generate(component_prompts)
 		return list(map(lambda comp: comp[0].text, generated_code.generations))
-
-	def generate_pages_code(self, pages: list[str], usable_components: list[str]):
-		# components = self.
-		return
 
 def extract_file_name_no_ext(full_name: str, extension: str) -> str:
 	return full_name.split(extension)[0]
@@ -67,12 +53,8 @@ def generate_components(agent: FrontendAgent, proj_tree: TreeNode) -> list[str]:
 async def main():
 	agent = FrontendAgent()
 	project_name = 'chat app'
-	# test_component_name = 'sidebar'
-	# component_template = f'a {test_component_name} for {project_name}'
 	proj_frame = agent.generate_project_structure(project_name)
-	print('LLM RES:')
-	print(proj_frame)
-	print('-------------')
+	
 	print('TREE')
 	project_tree = build_tree_from_indented_text(proj_frame)
 	project_tree.pre_order_traverse()
@@ -84,37 +66,24 @@ async def main():
 	print(tree_components)
 
 	# Generate components
-	comp_dir = file_manager.create_overwrite_directory('./temp', 'components')
-
-	# for component_name in tree_components:
-	# 	[component_code] = agent.generate_component_code([f'a {component_name} for {project_name}'])
-	# 	file_manager.create_file(comp_dir, f'{component_name}.js', component_code)
+	comp_dir_path = file_manager.create_overwrite_directory('./temp', 'components')
 
 	# * batch generate example
 	component_codes = agent.generate_component_code([f'a {component_name} for {project_name}'] for component_name in tree_components)
 	for component_code, component_name in zip(component_codes, tree_components):
-		file_manager.create_file(comp_dir, f'{component_name}.js', component_code)
+		file_manager.create_file(comp_dir_path, f'{component_name}.js', component_code)
 
 	# Aggregate into pages
 	pages_dir_subtree = project_tree.find_node_by_name(target_name='pages')
 	tree_pages = None
 	if pages_dir_subtree is not None:
 		tree_pages = generate_components(agent=agent, proj_tree=pages_dir_subtree)
-		print(tree_pages)
-		# ?MULTIPLE GENERATE
-		# page_components = agent.generate_pages_code(tree_pages, tree_components)
-		# ?OR CHAIN
+
 		gen_pages = await agent.pages_chain.abatch([{'page_name': curr_page, 'project_name': project_name, 'components': tree_components} for curr_page in tree_pages])
 		page_dir = file_manager.create_overwrite_directory('./temp', 'pages')
 		for page_code, page_name in zip(gen_pages, tree_pages):
 			file_manager.create_file(page_dir, f'{page_name}.js', page_code)
-			print(page_code)
 
-
-	# * SINGLE INVOKE EXAMPLE
-	# print(agent.pages_chain.input_schema.schema())
-	if tree_pages is not None:
-		print(agent.pages_chain.invoke({'page_name': tree_pages[0], 'project_name': project_name, 'components': tree_components}))
 
 if __name__ == '__main__':
   	asyncio.run(main())
